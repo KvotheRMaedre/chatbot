01a70a9a6075bc71db94f9b9e09bcca712d8a672
Ítalo H. <italodssouza.backup@gmail.com>
2025-05-13 10:32:19 -0300

    Bot presenting itself to the user

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,10 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        String botName = "Kvothe";
+        int botBirthdayYear = 2025;
+
+        System.out.println("Hello! My name is " + botName + ".");
+        System.out.println("I was created in " + botBirthdayYear + ".");
     }
 }
